e13bef1afed7d5724e7b1aa3f5c439283da5a27e
CEASLEY803 <ColeEasley90@gmail.com>
2025-03-08 14:11:04 -0500

    Ran the app to ensure that it is functional and added comments to ensure that I understand the code.

--- hello_world/src/hello_world_app.erl
+++ hello_world/src/hello_world_app.erl
@@ -4,10 +4,19 @@
 
 -export([start/2, stop/1]).
 
+%% start/2 means the function takes two arguments
+%%   StartType: Indicates how the application is started (e.g., 'normal' or during a restart).
+%%   StartArgs: A list of arguments that can be used during startup.
+%% The _ means that the variable is not used intentionally
 start(_StartType, _StartArgs) ->
+    %% io:format to write to the console, ~n to start a new line
     io:format("Hello, World!~n"),
+    %% return {ok, self()} to indicate successful startup
     {ok, self()}.
 
+%% stop/1 means the function takes one argument
+%% State: The state of the application provided by Erlang.
 stop(_State) ->
+    %% return ok to indicate that it closed successfully
     ok.
 
