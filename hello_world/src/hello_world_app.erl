ffffa78ceea3117918f3d3b33988f2d6b140cc62
CEASLEY803 <ColeEasley90@gmail.com>
2025-03-08 14:11:46 -0500

    Changed minor text indentation issue that was bothering me.

--- hello_world/src/hello_world_app.erl
+++ hello_world/src/hello_world_app.erl
@@ -5,8 +5,8 @@
 -export([start/2, stop/1]).
 
 %% start/2 means the function takes two arguments
-%%   StartType: Indicates how the application is started (e.g., 'normal' or during a restart).
-%%   StartArgs: A list of arguments that can be used during startup.
+%%  StartType: Indicates how the application is started (e.g., 'normal' or during a restart).
+%%  StartArgs: A list of arguments that can be used during startup.
 %% The _ means that the variable is not used intentionally
 start(_StartType, _StartArgs) ->
     %% io:format to write to the console, ~n to start a new line
@@ -15,7 +15,7 @@ start(_StartType, _StartArgs) ->
     {ok, self()}.
 
 %% stop/1 means the function takes one argument
-%% State: The state of the application provided by Erlang.
+%%  State: The state of the application provided by Erlang.
 stop(_State) ->
     %% return ok to indicate that it closed successfully
     ok.
