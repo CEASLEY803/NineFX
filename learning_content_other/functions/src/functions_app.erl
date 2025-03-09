49041ad21afff7dcf021f71d68a3c4e9bc7935de
CEASLEY803 <ColeEasley90@gmail.com>
2025-03-09 18:28:31 -0400

    added a context menu when you open the rebar3 shell

--- learning_content_other/functions/src/functions_app.erl
+++ learning_content_other/functions/src/functions_app.erl
@@ -10,7 +10,12 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    io:format("Available apps:~n"),
+    io:format("1. greetings:greet(gender, name in quotes)- enter gender in lowercase~n"),
+    io:format("2. basic_recursion:factorial(N)~n"),
+    io:format("3. pixels:extract_rgb(Pixels)- Ex: <<255, 0, 0, 0, 255, 0, 0, 0, 255>>~n"),
     functions_sup:start_link().
+
 
 stop(_State) ->
     ok.
