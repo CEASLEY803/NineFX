69cc3fae8977c51898158954bb81b2677e0a4436
CEASLEY803 <ColeEasley90@gmail.com>
2025-03-12 14:19:57 -0400

    expanded the monitor demo to ensure I understand everything properly.

--- learning_content_other/src/process_monitoring/monitor_demo.erl
+++ learning_content_other/src/process_monitoring/monitor_demo.erl
@@ -2,9 +2,16 @@
 -export([start/0, worker/0]).
 
 start() ->
+    %% assign the spawned function's PID to our Pid variable, can be very helpful but I dont end up using it here
     Pid = spawn(fun worker/0),
+    %% next, assign a reference to the new monitor im creating, need it for my recieve block
+    %% process tells erlang what kind of object it's monitoring 
+    %% (which is a process in this case. based on what ive read there are lots of different things you can enter here, like port for example)
     Ref = erlang:monitor(process, Pid),
+    % Wait for a 'DOWN' message which will be sent when the monitored process terminates.
     receive
+        %% '_' before Pid because I will not be using it later.
+        %% in this case, DOWN needs 5 parameters {'DOWN', Ref, process, Pid, Reason}. Again, though we dont end up using the Pid.
         {'DOWN', Ref, process, _Pid, Reason} ->
             io:format("Monitored process died with reason: ~p~n", [Reason])
     end.
